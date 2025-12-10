a3feaef4a78ed6e75a4fb9b90dc322cf45f33332
Ansh Bajaj <anshbajaj836@gmail.com>
2025-12-10 15:26:26 +0530

    setting up keyvalue store config

---
+++ node-app/src/main/java/com/keyvalue/Node.java
@@ -0,0 +1,21 @@
+//package com.keyvalue;
+//
+//import io.javalin.Javalin;
+//import okhttp3.*;
+//import java.io.IOException;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.concurrent.TimeUnite;
+//
+//public class Node {
+//
+//    // Shared HTTP Client
+//    private static final OkHttpClient client = new OkHttpClient.Builder()
+//            .readTimeout(2, TimeUnit.SECONDS)
+//            .build();
+//
+//
+//
+//
+//
+//}
